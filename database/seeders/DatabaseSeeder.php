356095e30895ecbbe50ff8c344daccbb7bb6960b
Huyen <huyenptt@htvietnam.vn>
2024-01-08 14:12:05 +0700

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,24 +22,24 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         DB::table("users")->truncate();
-      
+
         DB::table("users")->insert([
             'name' => 'admin',
             'username' => 'admin',
             'email' => 'admin@gmail.com',
-            'password'=> bcrypt('12345678'),
+            'password' => bcrypt('12345678'),
             'user_type' => 0,
             'email_verified_at' => Carbon::now(),
-            'status'=> 1
+            'status' => 1
         ]);
         DB::table("users")->insert([
             'name' => 'hellobabie',
             'username' => 'huyen pham',
             'email' => 'thanhuyen191@gmail.com',
-            'password'=> bcrypt('12345678'),
+            'password' => bcrypt('12345678'),
             'user_type' => 1,
             'email_verified_at' => Carbon::now(),
-            'status'=> 1
+            'status' => 1
         ]);
         DB::table('quiz_groups')->insert(
             ['name' => 'Câu hỏi 1'],
@@ -51,62 +51,10 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Câu hỏi 3'],
         );
         DB::table('news')->truncate();
-        DB::table('news')->insert([
-            [
-                'name' => 'Sample News 1',
-                'image' => 'sample_image_1.jpg',
-                'is_views' => 100,
-                'description' => 'Description of Sample News 1',
-                'content' => 'Content of Sample News 1',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Sample News 2',
-                'image' => 'sample_image_2.jpg',
-                'is_views' => 150,
-                'description' => 'Description of Sample News 2',
-                'content' => 'Content of Sample News 2',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Sample News 3',
-                'image' => 'sample_image_2.jpg',
-                'is_views' => 150,
-                'description' => 'Description of Sample News 2',
-                'content' => 'Content of Sample News 2',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Sample News 4',
-                'image' => 'sample_image_2.jpg',
-                'is_views' => 150,
-                'description' => 'Description of Sample News 2',
-                'content' => 'Content of Sample News 2',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Sample News 5',
-                'image' => 'sample_image_2.jpg',
-                'is_views' => 150,
-                'description' => 'Description of Sample News 2',
-                'content' => 'Content of Sample News 2',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Sample News 6',
-                'image' => 'sample_image_2.jpg',
-                'is_views' => 150,
-                'description' => 'Description of Sample News 2',
-                'content' => 'Content of Sample News 2',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        
-        ]);
+        for ($i = 0; $i < 5; $i++) {
+            DB::table('news')->insert([
+                ['name' => 'Sample News' . $i, 'image' => 'sample_image_1.jpg', 'is_views' => 0, 'description' => 'Description of Sample News 1', 'content' => 'Content of Sample News 1', 'created_at' => now(), 'updated_at' => now()],
+            ]);
+        }
     }
 }
