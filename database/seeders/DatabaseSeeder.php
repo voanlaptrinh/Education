ac87c3577b48bab6bf45e3c9539d219c123a46b4
Huyen <huyenptt@htvietnam.vn>
2024-01-08 13:56:13 +0700

    commit tao seeder news

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,6 +32,15 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => Carbon::now(),
             'status'=> 1
         ]);
+        DB::table("users")->insert([
+            'name' => 'hellobabie',
+            'username' => 'huyen pham',
+            'email' => 'thanhuyen191@gmail.com',
+            'password'=> bcrypt('12345678'),
+            'user_type' => 1,
+            'email_verified_at' => Carbon::now(),
+            'status'=> 1
+        ]);
         DB::table('quiz_groups')->insert(
             ['name' => 'Câu hỏi 1'],
         );
@@ -41,6 +50,63 @@ class DatabaseSeeder extends Seeder
         DB::table('quiz_groups')->insert(
             ['name' => 'Câu hỏi 3'],
         );
-       
+        DB::table('news')->truncate();
+        DB::table('news')->insert([
+            [
+                'name' => 'Sample News 1',
+                'image' => 'sample_image_1.jpg',
+                'is_views' => 100,
+                'description' => 'Description of Sample News 1',
+                'content' => 'Content of Sample News 1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Sample News 2',
+                'image' => 'sample_image_2.jpg',
+                'is_views' => 150,
+                'description' => 'Description of Sample News 2',
+                'content' => 'Content of Sample News 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Sample News 3',
+                'image' => 'sample_image_2.jpg',
+                'is_views' => 150,
+                'description' => 'Description of Sample News 2',
+                'content' => 'Content of Sample News 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Sample News 4',
+                'image' => 'sample_image_2.jpg',
+                'is_views' => 150,
+                'description' => 'Description of Sample News 2',
+                'content' => 'Content of Sample News 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Sample News 5',
+                'image' => 'sample_image_2.jpg',
+                'is_views' => 150,
+                'description' => 'Description of Sample News 2',
+                'content' => 'Content of Sample News 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Sample News 6',
+                'image' => 'sample_image_2.jpg',
+                'is_views' => 150,
+                'description' => 'Description of Sample News 2',
+                'content' => 'Content of Sample News 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        
+        ]);
     }
 }
