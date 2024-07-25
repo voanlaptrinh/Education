02d01136cd3216eb49594e3f793b593c2dd07bb8
khanh <voanlaptrinh@gmail.com>
2024-07-25 13:58:35 +0700

    aa

--- resources/views/admin/teams/detail.blade.php
+++ resources/views/admin/teams/detail.blade.php
@@ -1,30 +1,30 @@
 @extends('admin.index')
 @section('contentadmin')
- <section class="section">
+ <section class="section page-content-wrapper border">
         <div class="row">
-            <div class="col-xl-4">
+            <div class="col-lg-4 " style="">
 
                 <div class="card">
                     <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
                         <img style="width: 100px; height: 100px;" src="{{ asset('storage/' . $teamsItem->image) }}"
                             style="" alt="Profile" class="rounded-circle">
                         <h2>{{ $teamsItem->name }}</h2>
-                        <h6>{{ $teamsItem->created_at }}</h6>
+                        <h6>{{ $teamsItem->cuisine }}</h6>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-8">
+            <div class="col-lg-8">
 
                 <div class="card">
                     <div class="card-body pt-3">
                         <!-- Bordered Tabs -->
                         <ul class="nav nav-tabs nav-tabs-bordered" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview"
+                            <li class="nav-item  w-50" role="presentation">
+                                <button class="nav-link  w-100 active" data-bs-toggle="tab" data-bs-target="#profile-overview"
                                     aria-selected="true" role="tab">Mô tả ngắn</button>
                             </li>
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit"
+                            <li class="nav-item  w-50" role="presentation">
+                                <button class="nav-link  w-100" data-bs-toggle="tab" data-bs-target="#profile-edit"
                                     aria-selected="false" tabindex="-1" role="tab">Nội dung</button>
                             </li>
                         </ul>
